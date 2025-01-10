13e6e5a9eb20cd51a97f935739a7836cd59845bf
Your Name~ <hmphtetminpaing@gmail.com>
2025-01-10 10:51:23 +0630

    pom

---
+++ src/main/java/com/example/petclinicDB/PetClinicDBApplication.java
@@ -0,0 +1,13 @@
+package com.example.petclinicDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicDBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetClinicDBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/petclinicDB/PetClinicDBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petclinicDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicDBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
