757b946422166220b6aefdd11435dcb956bdebed
Your Name~ <hmphtetminpaing@gmail.com>
2025-01-21 14:26:12 +0630

    vets mapper

---
+++ src/main/java/com/example/petclinicDB/mapper/impl/VetMapper.java
@@ -0,0 +1,25 @@
+package com.example.petclinicDB.mapper.impl;
+
+import com.example.petclinicDB.domain.dto.VetDto;
+import com.example.petclinicDB.domain.entity.VetEntity;
+import com.example.petclinicDB.mapper.Mapper;
+import org.modelmapper.ModelMapper;
+
+public class VetMapper implements Mapper<VetEntity, VetDto> {
+
+    private ModelMapper modelMapper;
+
+    public VetMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+    @Override
+    public VetEntity mapToEntity(VetDto vetDto) {
+        return modelMapper.map(vetDto, VetEntity.class);
+    }
+
+    @Override
+    public VetDto mapToDto(VetEntity vetEntity) {
+        return modelMapper.map(vetEntity, VetDto.class);
+    }
+}
